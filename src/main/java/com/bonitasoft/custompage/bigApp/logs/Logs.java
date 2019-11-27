f2d1300e18f16ff2fd433d55b46a100732cdfca6
Ismail Lagouilly <i.lagouilly@gmail.com>
2019-11-26 16:23:44 -0800

    [ILA] Add class to manage Logs

---
+++ src/main/java/com/bonitasoft/custompage/bigApp/logs/Logs.java
@@ -0,0 +1,34 @@
+package com.bonitasoft.custompage.bigApp.logs;
+
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class Logs {
+
+    public static Map<String, Object> getLogs() throws IOException {
+
+        String dir = System.getProperty("catalina.home");
+
+        Set<String> fileList = new HashSet<>();
+        Map<String, Object> result = new HashMap<String, Object>();
+        String i = "";
+
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(Paths.get(dir+"/logs/"))) {
+            for (Path path : stream) {
+                if (!Files.isDirectory(path)) {
+                    fileList.add(path.getFileName().toString());
+                    i = i+ "\n "+ path.getFileName().toString();
+                }
+            }
+        }
+        result.put( "test", i);
+        return result;
+    }
+}
