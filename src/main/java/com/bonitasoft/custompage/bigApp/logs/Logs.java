08b0eac07506d9ebd597ae71c9da170eaea8eb19
Ismail Lagouilly <i.lagouilly@gmail.com>
2019-12-23 18:35:55 -0800

    [ILA] Fix Pom.xml version of bonita-serverconfiguration dependency

--- src/main/java/com/bonitasoft/custompage/bigApp/logs/Logs.java
+++ src/main/java/com/bonitasoft/custompage/bigApp/logs/Logs.java
@@ -1,5 +1,6 @@
 package com.bonitasoft.custompage.bigApp.logs;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
@@ -19,7 +20,7 @@ public class Logs {
             dir = System.getProperty( "jboss.server.log.dir" ) + "/";
         }
 
-        Set<String> fileList = new HashSet<>();
+        /*Set<String> fileList = new HashSet<>();
         List<Map<String, Object>> result = new ArrayList<Map<String, Object>>();
         Integer j = 0;
         try (DirectoryStream<Path> stream = Files.newDirectoryStream( Paths.get( dir ) )) {
@@ -30,6 +31,33 @@ public class Logs {
                     result.add( mapFile );
                     mapFile.put( "name", path.getFileName().toString() );
                 }
+            }
+        }*/
+
+        List<Map<String, Object>> result = new ArrayList<Map<String, Object>>();
+        File directory = new File(dir);
+        File[] filesArray = directory.listFiles();
+        //sort all files
+        /*Arrays.sort(filesArray, new Comparator<File>() {
+            public int compare(File f1, File f2) {
+                return Long.compare(f1.lastModified(), f2.lastModified());
+            }
+        }.reversed());*/
+        Comparator.comparingLong(File::lastModified).reversed();
+        Arrays.sort(filesArray, Collections.reverseOrder());
+        //print the sorted values
+        for (File file : filesArray) {
+            if(file.getName().contains( "bonita" )) {
+                Map<String, Object> mapFile = new HashMap<String, Object>();
+                result.add( mapFile );
+                mapFile.put( "name", file.getName() );
+            }
+        }
+        for (File file : filesArray) {
+            if(!file.getName().contains( "bonita" )) {
+                Map<String, Object> mapFile = new HashMap<String, Object>();
+                result.add( mapFile );
+                mapFile.put( "name", file.getName() );
             }
         }
 
